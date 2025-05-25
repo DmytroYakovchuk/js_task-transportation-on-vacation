cf66d6dc199223de23ecdf5d6612f131a065404b
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-05-25 16:24:23 +0300

    first js commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let sumPaid = 0;
+  const oneDay = 40;
+  const returnMoneyThreeDays = 20;
+  const returnMoneySevenDays = 50;
+
+  if (days <= 2) {
+    sumPaid = oneDay * days;
+  }
+
+  if (days >= 3 && days < 7) {
+    sumPaid = oneDay * days - returnMoneyThreeDays;
+  }
+
+  if (days >= 7) {
+    sumPaid = oneDay * days - returnMoneySevenDays;
+  }
+
+  return sumPaid;
 }
 
 module.exports = calculateRentalCost;
